3012ddbf2a3b639f4e27790d8f7191dc52aab16a
Riti16 <ritidass29@gmail.com>
2020-03-28 21:50:44 +0530

    start

--- Covid_Predictions_viz.py
+++ Covid_Predictions_viz.py
@@ -97,16 +97,15 @@ app.layout = html.Div([
     html.P("* 3 initial cases in Kerala recovered; the above chart shows actual covid numbers from the resumption of cases on March 2nd and predicted covid numbers from March 26th.", style={'fontSize': 10}),
     html.P("* Actual Covid Numbers are taken from Ministry Of Health And Family Welfare Covid Website : https://covidindia.org/", style={'fontSize': 10}),
     html.P("* The above Coronavirus spread numbers are predicted using log transformations, Exponential Growth and linear regression. The Linear Model is only the best estimate of the Exponential Growth function.", style={'fontSize': 10}),
-    html.P("* Possibilities of going further:"), style={'fontSize': 10},
+    html.P("* Possibilities of going further:", style={'fontSize': 10}),
     html.Ol([
         html.Li([
                   "The Exponential Growth will only fit the epidemic at the beginning. Due to measures such as social distancing and lockdown, the growth might not follow exponential pattern or there could be certain error margin that we could inspect in further study.",
-               ], style={'fontSize': 10}),
-        html.Li(["Also, at some point, healed people will not spread the virus anymore and when (almost) everyone is or has been infected, the growth will stop.", style={'fontSize': 10}], style={'fontSize': 10})
-    ]),
+               ] , style={'fontSize': 10}),
+        html.Li(["Also, at some point, healed people will not spread the virus anymore and when (almost) everyone is or has been infected, the growth will stop."])
+    ], style={'fontSize': 10}),
     html.P("NOTE: The exponential growth of Corona Virus outbreak is limited to the first phase of the outbreak since the big limitation of Exponential Growth is that it never stops growing. Later, we'll switch to the next step for this model as the epidemic growth is characterized by increasing growth in the beginning period (Epidemiologists have studied these types of outbreaks and it is well known that the first period of an epidemic follows Exponential Growth), but a decreasing growth at a later stage. For example in the Coronavirus case, this maximum limit would be the total number of people in the world, because when everybody is sick, the growth will necessarily diminish.", style={'color': 'DarkBlue', 'fontSize': 10}),
     html.P("Thanks for visting. The source code is on Github which will be made public soon and pull requests will also be accepted in sometime. Stay tuned:)", style={'color': 'DarkGoldenRod', 'fontSize': 10})
 ])
-
 if __name__ == '__main__':
     app.run_server(debug=True)
