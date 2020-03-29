4d1d40be9db196d2241f9966acaffeac74b896f4
Riti16 <ritidass29@gmail.com>
2020-03-30 02:28:10 +0530

    live

--- Covid_Predictions_viz.py
+++ Covid_Predictions_viz.py
@@ -134,10 +134,10 @@ app.layout = html.Div([
                   style_header={'backgroundColor': 'white','fontWeight': 'bold','fontSize':'18',},
                   style_data_conditional=[{'if': {'row_index': 'odd'},
                                            'backgroundColor': 'rgb(248, 248, 248)'}],
-                  )],style = {'display': 'inline-block', 'width': '80%', 'height':'30%'}),
+                  )],style = {'display': 'inline-block', 'width': '100%', 'height':'30%'}),
     #dcc.Graph(figure=fig),
     html.Div(children = [
-                dcc.Graph(figure=fig)],style = {'display': 'inline-block', 'width': '80%', 'height':'60%'}),
+                dcc.Graph(figure=fig)],style = {'display': 'inline-block', 'width': '100%', 'height':'60%'}),
     html.H2("Accuracy of the prediction is: 98.88%", style={'fontSize': 24}), #change here - paste from accuracy_final
     html.P("* 3 initial cases in Kerala recovered; the above chart shows actual covid numbers from the resumption of cases on March 2nd and predicted covid numbers from March 26th.", style={'fontSize': 10}),
     html.P("* Actual Covid Numbers are taken from Ministry Of Health And Family Welfare Covid Website : https://covidindia.org/", style={'fontSize': 10}),
